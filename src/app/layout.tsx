db7f92e0bd97d1c0c3b2b6c718d2883395b420f9
asaga22 <lharaskinanti@gmail.com>
2025-03-06 21:46:08 +0700

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,22 @@ import Footer from "./components/footer";
 export const metadata: Metadata = {
   title: "YourLifeChoices - Best digital publication",
   description: "YourLifeChoices is Australia's most established and trusted digital publication for the 50+ audience, with a core focus on helping Australians navigate midlife and the retirement landscape.",
-  icons: {
-    icon: "/favicon.ico",
+  icons: "favicon.ico",
+  viewport: "width=device-width, initial-scale=1",
+  themeColor: "#FFFFFF",
+  twitter: {
+    card: "summary",
+    site: "@yourlifechoices",
+    creator: "@yourlifechoices",
   },
+  openGraph: {
+    type: "website",
+    locale: "en_AU",
+    url: "https://www.yourlifechoices.com.au",
+    siteName: "YourLifeChoices",
+    title: "YourLifeChoices - Best digital publication",
+    description: "YourLifeChoices is Australia's most established and trusted digital publication for the 50+ audience, with a core focus on helping Australians navigate midlife and the retirement landscape.",
+  }
 };
 
 export default function RootLayout({
